6dda270d0f4722084c3a9cad88e8c3fb60a9d0cc
lahiru <lahirunisaluor123@gmail.com>
2025-01-17 12:24:05 +0530

    create powerpoint and code for click events

--- NetNinjaReact/dojo-blog/src/Home.js
+++ NetNinjaReact/dojo-blog/src/Home.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
 export const Home = () => {
+
+  const handleClick = (e) => {
+    console.log("hello ", e);
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log("hello " + name, e.target);
+  }
   return (
-    <div>Home Page</div>
+    <div>
+      <h2>Home Page</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain('lahiru', e)}>Click me Again</button>
+    </div>
   )
 }
