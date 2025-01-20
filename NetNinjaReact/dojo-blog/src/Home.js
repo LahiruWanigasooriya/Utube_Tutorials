4f54f409e50f769b460ed69b8703b50e3f5f24ee
lahiru <lahirunisaluor123@gmail.com>
2025-01-21 01:32:52 +0530

    reusing components created

--- NetNinjaReact/dojo-blog/src/Home.js
+++ NetNinjaReact/dojo-blog/src/Home.js
@@ -7,12 +7,13 @@ export const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: "My first blog", body: "This is my first blog", author: "Lahiru", id: 1 },
     { title: "My second blog", body: "This is my second blog", author: "Udeshika", id: 2 },
-    { title: "My third blog", body: "This is my third blog", author: "Ula", id: 3 }
+    { title: "My third blog", body: "This is my third blog", author: "Lahiru", id: 3 }
   ])
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs"/>
+      <BlogList blogs={blogs.filter((blog)=>blog.author==="Lahiru")} title="Lahiru's Blogs"/>
     </div>
   );
 }
