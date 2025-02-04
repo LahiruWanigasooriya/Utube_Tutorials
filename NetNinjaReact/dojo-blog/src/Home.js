bb02753ea3538c3fa202ecd47d56165018c9eb19
lahiru <lahirunisaluor123@gmail.com>
2025-02-04 21:43:12 +0530

    usinf jason server created

--- NetNinjaReact/dojo-blog/src/Home.js
+++ NetNinjaReact/dojo-blog/src/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import BlogList from './BlogList';
 
 export const Home = () => {
@@ -10,15 +10,24 @@ export const Home = () => {
     { title: "My third blog", body: "This is my third blog", author: "Lahiru", id: 3 }
   ]);
 
+  const [name, setName] = useState('lahiru');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
 
+  useEffect(() => {
+    console.log("useEffect ran on");
+    console.log(name);
+  }, [name]);
+
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
       <BlogList blogs={blogs.filter((blog) => blog.author === "Lahiru")} title="Lahiru's Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('udeshika')}>Change Name</button>
+      <p>{name}</p>
     </div>
   );
 }
