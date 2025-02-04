46617338d51adef0854f9558169c721e46933971
lahiru <lahirunisaluor123@gmail.com>
2025-02-04 22:09:03 +0530

    update Fetching Data with useEffect

--- NetNinjaReact/dojo-blog/src/BlogList.js
+++ NetNinjaReact/dojo-blog/src/BlogList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const BlogList = ({blogs,title,handleDelete}) => {
+const BlogList = ({blogs,title}) => {
 
     return (
         <div className="blog-list">
@@ -10,7 +10,6 @@ const BlogList = ({blogs,title,handleDelete}) => {
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <button>View Blog</button>
-                    <button onClick={()=>handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
 
--- NetNinjaReact/dojo-blog/src/Home.js
+++ NetNinjaReact/dojo-blog/src/Home.js
@@ -4,28 +4,27 @@ import BlogList from './BlogList';
 
 export const Home = () => {
 
-  const [blogs, setBlogs] = useState([
-    { title: "My first blog", body: "This is my first blog", author: "Lahiru", id: 1 },
-    { title: "My second blog", body: "This is my second blog", author: "Udeshika", id: 2 },
-    { title: "My third blog", body: "This is my third blog", author: "Lahiru", id: 3 }
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const [name, setName] = useState('lahiru');
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+
 
   useEffect(() => {
-    console.log("useEffect ran on");
-    console.log(name);
+    fetch('http://localhost:8000/blogs')
+    .then(res=>{
+      return res.json();
+    })
+    .then(data=>{
+      setBlogs(data);
+    }
+    )
   }, [name]);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <BlogList blogs={blogs.filter((blog) => blog.author === "Lahiru")} title="Lahiru's Blogs" handleDelete={handleDelete} />
+      {blogs && <BlogList blogs={blogs} title="All Blogs"  />}
+      {/* <BlogList blogs={blogs.filter((blog) => blog.author === "Lahiru")} title="Lahiru's Blogs" handleDelete={handleDelete} /> */}
       <button onClick={() => setName('udeshika')}>Change Name</button>
       <p>{name}</p>
     </div>
