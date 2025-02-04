f95499414b8272901726eb9b37e5e604a4f0c97d
lahiru <lahirunisaluor123@gmail.com>
2025-02-05 00:43:51 +0530

    Error handling functions created

--- NetNinjaReact/dojo-blog/src/Home.js
+++ NetNinjaReact/dojo-blog/src/Home.js
@@ -6,25 +6,35 @@ export const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
+      fetch('http://localhost:8000/blogssss')
         .then(res => {
+          if (!res.ok) {
+            throw Error('could not fetch data for the resource');
+          }
           return res.json();
         })
         .then(data => {
           setBlogs(data);
           setIsLoading(false);
-        });
-    },1000);
+          setError(null);
+        })
+        .catch(err => {
+          setError(err.message);
+          setIsLoading(false);
+        })
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
-      </div>
+    </div>
   );
 }
 
