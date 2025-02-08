98cb902dc9e97c63e2b936f92c5e8439baa227b7
lahiru <lahirunisaluor123@gmail.com>
2025-02-08 11:21:58 +0530

    create page created and connected

--- NetNinjaReact/dojo-blog/src/App.js
+++ NetNinjaReact/dojo-blog/src/App.js
@@ -1,16 +1,29 @@
 import './App.css';
+import Create from './Create';
 import { Home } from './Home';
 import { NavBar } from './NavBar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
 
   return (
-    <div className="App">
-      <NavBar/>
-      <div className="content">
-        <Home/>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+          
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- NetNinjaReact/dojo-blog/src/NavBar.js
+++ NetNinjaReact/dojo-blog/src/NavBar.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 export const NavBar = () => {
     return (
-    <nav className='navbar'>
-        <h1>Dojo Blog</h1>
-        <class className="link">
-            <a href="/">Home</a>
-            <a href="/create">New Blog</a>
-        </class>
-    </nav>
+        <nav className='navbar'>
+            <h1>Dojo Blog</h1>
+            <class className="link">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </class>
+        </nav>
     )
 }
