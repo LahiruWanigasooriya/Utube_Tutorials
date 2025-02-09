9f9ad92e805242285f617834e6959622f8b2902c
lahiru <lahirunisaluor123@gmail.com>
2025-02-09 10:20:02 +0530

    create link routes

--- NetNinjaReact/dojo-blog/src/NavBar.js
+++ NetNinjaReact/dojo-blog/src/NavBar.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const NavBar = () => {
     return (
         <nav className='navbar'>
             <h1>Dojo Blog</h1>
             <class className="link">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
             </class>
         </nav>
     )
 }
+
+
